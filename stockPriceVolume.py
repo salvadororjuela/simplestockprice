cb33c98d1fefdb378c616723e88b4278d809086f
Salvador Orjuela <salvadororjuela@gmail.com>
2024-02-08 01:10:52 +0000

    "Hello.py deleted, stockPriceVolume.py name changed"

---
+++ stockPriceVolume.py
@@ -0,0 +1,25 @@
+import yfinance as yf
+import streamlit as st 
+import pandas as pd
+
+st.write("""
+         # Simple Stock Price App
+         
+         Shown are the stock ***closing*** price and **volume** of Google
+
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+
+# Define the ticker symbol
+tickerSymbol = "GOOGL"
+
+# Get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# Get the historical pricer for this ticker
+tickerDF = tickerData.history(period="1d", start= "2010-5-31", end="2020-5-31")
+
+# Open high Low Close Volume Dividends Stock Splits
+st.line_chart(tickerDF.Close)
+st.line_chart(tickerDF.Volume)
